76241378d362ea7d95603eb8528ba9c1170a34fc
kapskapskaps <kirilllov1402@gmail.com>
2026-02-19 16:56:07 +0300

    new govnocode

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,7 @@ app = FastAPI(title="Tutor CRM", version="1.0.0", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "http://193.222.99.69:3000", "http://193.222.99.69"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
